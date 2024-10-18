d6a06beddaf3017868838d85c60cc163cda614fb
dev-taeho <cth6809@daum.net>
2024-10-18 13:47:42 -0500

    feat: setup nestia service

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,16 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
-  const docsConfig = new DocumentBuilder()
-  .setTitle('akcse-mb')
-  .setDescription('Akcse MB API Document')
-  .setVersion('1.0')
-  .addServer('/api/v1')
-  .build()
-  const docs = SwaggerModule.createDocument(app, docsConfig)
-
   const config = app.get(ConfigService);
   const url = config.get<string>('API_SERVER_URL') as string;
   const port = config.get<number>('API_SERVER_PORT') as number;
   // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const docs = require('../../packages/api/swagger.json');
+  docs.servers = [
+    {
+      url: `${url}`,
+    },
+  ];
   SwaggerModule.setup('api', app, docs);
   await app.listen(port);
 }
