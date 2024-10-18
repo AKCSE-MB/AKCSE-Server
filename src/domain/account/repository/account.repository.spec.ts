7260f8e31517f0c6a8f55f3ebc8fc53dd5c81a6f
dev-taeho <cth6809@daum.net>
2024-10-18 18:40:30 -0500

    feat: impl account repository layer

---
+++ src/domain/account/repository/account.repository.spec.ts
@@ -0,0 +1,35 @@
+import { truncateTables } from '@root/jest.setup';
+import prismaClient from '@common/database/prisma';
+import {
+    getIdentification,
+  saveAccount,
+  saveAccounts
+} from '@domain/account/repository/account.repository';
+
+describe('account repository', () => {
+  beforeEach(async () => {
+    await truncateTables(prismaClient, ['accounts']);
+  });
+
+  it('should save account', async () => {
+    const data = {
+      identification: 'some@email.com',
+      password: 'one-way-decoded-password',
+    };
+    await saveAccount(data);
+    const account = await getIdentification(data.identification);
+    expect(account).not.toBeNull();
+  });
+
+  it('should save accounts', async () => {
+    const data = [
+      {
+        identification: 'some@email.com',
+        password: 'one-way-decoded-password',
+      },
+    ];
+    await saveAccounts(data);
+    const account = await getIdentification(data[0].identification);
+    expect(account).not.toBeNull();
+  });
+});
---
+++ src/domain/account/repository/account.repository.ts
@@ -0,0 +1,26 @@
+import prismaClient from "@common/database/prisma";
+
+
+
+export async function saveAccount(param: {
+    identification: string;
+    password: string;
+}) {
+    await saveAccounts([param]);
+}
+
+export async function saveAccounts(param: {
+    identification: string;
+    password: string;
+}[]) {
+    return prismaClient.accounts.createMany({data: param});
+}
+
+export async function getIdentification(identification: string) {
+    return prismaClient.accounts.findFirst({
+        where: {
+            identification: identification
+        }
+    })
+}
+
