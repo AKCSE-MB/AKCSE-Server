333f630f1f4be6d06b21702668bc2167651806ba
dev-taeho <cth6809@daum.net>
2024-10-18 19:13:52 -0500

    feat: add account module to main

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LoggingModule } from '@src/common/logging/logging.module';
 import { ConfigurationModule } from '@domain/configuration/configuration.module';
+import { AccountModule } from '@domain/account/account.module';
 import { validate } from '@src/env.validation';
 
 @Module({
   imports: [
     ConfigurationModule,
+    AccountModule,
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
