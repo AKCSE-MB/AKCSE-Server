2a8b3b4df65501a6438ada40ce1434b2d683cf39
Jade Lee <jadelee0924@gmail.com>
2024-10-31 21:11:38 -0500

    fix: add program column to members table

--- prisma/migrations/20241025023401_members_table/migration.sql
+++ prisma/migrations/20241025023401_members_table/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "members" (
     "num_attend" INTEGER NOT NULL,
     "name" VARCHAR(256) NOT NULL,
     "username" VARCHAR(256) NOT NULL,
+    "program" VARCHAR(256) NOT NULL, 
     "role" VARCHAR(256) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
