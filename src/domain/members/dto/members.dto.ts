be925007d4b0d7aee201faf7fd4a18431d68af2c
Jade Lee <131495939+JSLee-24@users.noreply.github.com>
2024-11-26 21:53:46 -0600

    Members dto (#33)
    * feat: implement members dtos
    
    * feat: add a new dto to use when requesting an update
    
    * feat: add createdAt and updatedAt field for MemberResponseDTO
    
    * chore: rename dtos to match naming schemes
    
    * chore: replace the type for program and role with enumerated types
    
    * chore: update import source

---
+++ src/domain/members/dto/members.dto.ts
@@ -0,0 +1,38 @@
+import { Program, Role } from '@domain/members/members.enum';
+
+export interface MemberCreateRequestDTO {
+  score: number;
+  numAttend: number;
+  name: string;
+  username: string;
+  program: Program;
+  role: Role;
+}
+
+export interface MemberUpdateRequestDTO {
+  score?: number;
+  numAttend?: number;
+  name?: string;
+  username?: string;
+  program?: Program;
+  role?: Role;
+}
+
+export interface MembersResponseDTO {
+  id: number;
+  score: number;
+  numAttend: number;
+  name: string;
+  username: string;
+  program: Program;
+  role: Role;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface TopMemberResponseDTO {
+  id: number;
+  username: string;
+  score: number;
+  numAttend: number;
+}
