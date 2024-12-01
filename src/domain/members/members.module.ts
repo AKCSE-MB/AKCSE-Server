6ffc1ebb17bdf488a798886178107d36840b0afd
Jade Lee <jadelee0924@gmail.com>
2024-11-30 23:59:44 -0600

    chore: import ConfigurationModule into MembersModule

--- src/domain/members/members.module.ts
+++ src/domain/members/members.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationService } from '@domain/configuration/configuration.service';
 import { JwtService } from '@nestjs/jwt';
-import { MembersController } from './controller/members.controller';
+import { MembersController } from '@domain/members/controller/members.controller';
+import { ConfigurationModule } from '@domain/configuration/configuration.module';
 
 @Module({
+  imports: [ConfigurationModule],
   controllers: [MembersController],
   providers: [ConfigurationService, JwtService],
 })
