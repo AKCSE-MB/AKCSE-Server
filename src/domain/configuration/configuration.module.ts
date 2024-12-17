cd42bef008cb8754b9336f95f45669e4871cfff7
Jade Lee <jadelee0924@gmail.com>
2024-12-16 19:54:06 -0500

    chore: remove unused providers

--- src/domain/configuration/configuration.module.ts
+++ src/domain/configuration/configuration.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigurationService } from '@domain/configuration/configuration.service';
 import { V1ConfigurationController } from '@domain/configuration/configuration.controller';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [V1ConfigurationController],
-  providers: [ConfigurationService, ConfigService],
-  exports: [ConfigurationService, ConfigService],
+  providers: [ConfigurationService],
+  exports: [ConfigurationService],
 })
 export class ConfigurationModule {}
