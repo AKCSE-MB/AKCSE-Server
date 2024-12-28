af36e0586a193e1241e667ccf764485606a6866e
Jade Lee <131495939+JSLee-24@users.noreply.github.com>
2024-12-27 21:27:29 -0500

    feat: Implement Members Controller
    * feat: add a new dto to use when requesting an update
    
    * feat: implement members controller layer
    
    * feat: implement members module
    
    * chore: add MembersModule in app module
    
    * fix: replace repository layer fcn to service layer fcn
    
    * fix: specify type of data when converting to BaseResponseDto
    
    Fix GET functions
    
    * fix: change urls for REST to avoid conflicts
    
    * test: implement members controller test
    
    * chore: update dto imports
    
    * test: implement members controller test
    
    * chore: remove unused imports
    
    * fix: make create, update, and delete methods to return the resource
    
    * fix: fix the way to access returned object
    
    * fix: refactor member repository test
    
    * fix: make the type of program and role in the response dto to be string
    
    * chore: remove search fcn
    
    * chore: remove return value when creating a member
    
    * chore: update dtos in controller
    
    * chore: update imports
    
    * chore: replace all program and roles to enumerated typed values
    
    * chore: remove unused code
    
    * chore: update dto names
    
    * chore: replace all mockResolvedValue() to mockResolvedValueOnce()
    
    * feat: perform basic data check
    
    * chore: replace mockResolvedValue() with mockResolvedValueOnce()
    
    * chore: update HTTP request urls
    
    * chore: update import sources
    
    * chore: replace createLeaderboard() with getLeaderboard()
    
    * fix: remove an unecessary mock object
    
    * fix: properly access data from returned res
    
    * fix: adjust/simplify expect() statements
    
    * feat: truncate members table after testing creation.
    
    * fix: make all request dtos to specify the type of program and role to be the enumerated Program and Role type
    
    * chore: update import source
    
    * fix: remove db manipulating code by using mockImplementation()
    
    * fix: use a mock leaderboard instead of an mock implementation of the fcn
    
    * feat: use auth guards to protect routers
    
    * chore: remove unused imports
    
    * chore: import ConfigurationModule into MembersModule
    
    * chore: Add ConfigService as a provider and export it in ConfigurationModule
    
    * feat: override guard before each test to perform requests
    
    * feat: add member validation before update and deletion
    
    * fix: update summary of addMember()
    
    * fix: update GET controllers
    
    remove "top5" from request url for the leaderboard
    change order of get by id and leaderboard
    
    * fix: update controller tags
    
    * fix: remove auth guards from fcns that shouldn't have them
    
    * fix: update responses
    
    * chore: remove duplicate code
    
    * chore: update response
    
    * feat: pass token as a header for requests protected by a auth guard
    
    * chore: refactor members controller
    
    set all tags as members
    return a baseResponseDto instance in addMember()
    
    * feat: update port for the local host url and update swagger document
    
    * chore: refactor members controller test
    
    set tokens for only the functions that has an auth guard
    simplify variable names
    
    * chore: remove unused imports
    
    * chore: simplify duplication in DTOs by using Omit and Partial
    
    * chore: remove unused providers
    
    * chore: remove unecessary security decorators
    
    * chore: update swagger

---
+++ src/domain/members/controller/members.controller.ts
@@ -0,0 +1,111 @@
+import {
+  Controller,
+  HttpCode,
+  Injectable,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from '@common/exception/exception.filter';
+import { TypedBody, TypedRoute, TypedParam } from '@nestia/core';
+import {
+  MemberCreateRequestDTO,
+  MemberUpdateRequestDTO,
+  MembersResponseDTO,
+  TopMembersResponseDTO,
+} from '@domain/members/dto/members.dto';
+import {
+  getAllMembers,
+  getMember,
+  createMember,
+  editMember,
+  removeMember,
+  getLeaderboard,
+} from '@domain/members/service/members.service';
+import { BaseResponseDto } from '@common/dto/base.dto';
+import { AuthGuard } from '@common/auth/auth.guard';
+
+@Controller('v1/members')
+@UseFilters(new HttpExceptionFilter())
+@Injectable()
+export class MembersController {
+  /**
+   * @tag members
+   * @summary create a new member
+   * @security bearer
+   */
+  @UseGuards(AuthGuard)
+  @TypedRoute.Post()
+  @HttpCode(200)
+  async addMember(
+    @TypedBody() memberData: MemberCreateRequestDTO,
+  ): Promise<BaseResponseDto<object>> {
+    await createMember(memberData);
+    return new BaseResponseDto({ state: 'success' });
+  }
+
+  /**
+   * @tag members
+   * @summary get all registered members
+   */
+  @TypedRoute.Get()
+  @HttpCode(200)
+  async getMembers(): Promise<BaseResponseDto<MembersResponseDTO[]>> {
+    const res = await getAllMembers();
+    return new BaseResponseDto(res);
+  }
+
+  /**
+   * @tag members
+   * @summary get top 5 members with the highest scores
+   */
+  @TypedRoute.Get('/leaderboard')
+  @HttpCode(200)
+  async getLeaderboard(): Promise<BaseResponseDto<TopMembersResponseDTO[]>> {
+    const res = await getLeaderboard();
+    return new BaseResponseDto(res);
+  }
+
+  /**
+   * @tag members
+   * @summary get a member with the passed id
+   */
+  @TypedRoute.Get('/:id')
+  @HttpCode(200)
+  async getMember(
+    @TypedParam('id') id: number,
+  ): Promise<BaseResponseDto<MembersResponseDTO>> {
+    const res = await getMember(id);
+    return new BaseResponseDto(res);
+  }
+
+  /**
+   * @tag members
+   * @summary Update an existing member's details
+   * @security bearer
+   */
+  @UseGuards(AuthGuard)
+  @TypedRoute.Put('/:id')
+  @HttpCode(200)
+  async updateMember(
+    @TypedParam('id') id: number,
+    @TypedBody() memberData: MemberUpdateRequestDTO,
+  ): Promise<BaseResponseDto<MembersResponseDTO>> {
+    const res = await editMember(id, memberData);
+    return new BaseResponseDto(res);
+  }
+
+  /**
+   * @tag members
+   * @summary Delete a member
+   * @security bearer
+   */
+  @UseGuards(AuthGuard)
+  @TypedRoute.Delete('/:id')
+  @HttpCode(200)
+  async deleteMember(
+    @TypedParam('id') id: number,
+  ): Promise<BaseResponseDto<MembersResponseDTO>> {
+    const res = await removeMember(id);
+    return new BaseResponseDto(res);
+  }
+}
