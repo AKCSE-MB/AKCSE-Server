43ce20f2595bf1818a31b423a78a2f7d417b3492
orca277 <killerwhale2779@protonmail.com>
2025-02-23 14:06:57 -0600

    Updated aws-sdk
    Removed aws-sdk and installed @aws-sdk/client-s3

--- src/common/utils/s3.util.ts
+++ src/common/utils/s3.util.ts
@@ -1,19 +1,24 @@
-import { S3 } from 'aws-sdk';
+import {
+  S3Client,
+  PutObjectCommand,
+  ObjectCannedACL,
+} from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class S3Service {
-  private s3: S3;
+  private s3: S3Client;
   private bucketName: string;
 
   constructor(private readonly configService: ConfigService) {
-    this.s3 = new S3({
-      accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY'),
-      secretAccessKey: this.configService.get<string>('AWS_SECRET_KEY'),
+    this.s3 = new S3Client({
       region: this.configService.get<string>('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY') || '',
+        secretAccessKey: this.configService.get<string>('AWS_SECRET_KEY') || '',
+      },
     });
-
     this.bucketName = this.configService.get<string>('AWS_BUCKET_NAME') || '';
   }
 
@@ -27,10 +32,14 @@ export class S3Service {
       Key: `events/${Date.now()}-${file.originalname}`,
       Body: file.buffer,
       ContentType: file.mimetype,
-      ACL: 'public-read',
+      ACL: 'public-read' as ObjectCannedACL,
     };
 
-    const result = await this.s3.upload(params).promise();
-    return result.Location; // Returns the S3 URL
+    const command = new PutObjectCommand(params);
+    await this.s3.send(command);
+
+    return `https://${this.bucketName}.s3.${this.configService.get<string>(
+      'AWS_REGION',
+    )}.amazonaws.com/${params.Key}`;
   }
 }
